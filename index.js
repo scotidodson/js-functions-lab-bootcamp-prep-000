a97987e2d4927ad7c6bf36d8ed92827d3ab4a2f9
Scoti Dodson <scotidodson@gmail.com>
2018-07-04 13:48:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
+<<<<<<< HEAD
   return `Happy holidays, ${name}!`;
 }
 
@@ -12,4 +13,15 @@ function happyHolidayTo(holiday, name) {
 
 function holidayCountdown(holiday, days) {
   return `It\'s ${days} days until ${holiday}!`;
+=======
+  return 'Happy holidays, ${name}!';
+}
+
+function happyHolidayTo(holiday, name) {
+  return 'Happy ${holiday}, ${name}!';
+}
+
+function holidayCountdown(days, holiday) {
+  return 'It\'s ${days} days until ${holiday}!';
+>>>>>>> cb2401de16b564b243869b2bd5ec9a167caa8592
 }
